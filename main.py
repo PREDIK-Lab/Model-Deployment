53677f13add71c4da100bbdfaaf0d877c436d2b8
paulinagh <paulinanel50@gmail.com>
2023-01-23 22:28:15 +0700

    Main file configuration

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-from flask import Flask, jsonify
-import os
-
+from predict_model import *
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+@app.route("/")
+def greeting():
+    return "<h1 style='color:green'>Hello World!</h1>"
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/bantu")
+def help():
+    return "eid"
 
+@app.route("/prediksi", methods = ['GET'])
+def make():
+    result = predict_model()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    if(request.method == 'GET'):
+        data = {
+            "success" : True,
+            "result" : result,
+        }
+  
+        return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')

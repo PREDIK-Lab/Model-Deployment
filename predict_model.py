871a8b21cb0b9ff2408afe04459a8e34b5252915
paulinagh <paulinanel50@gmail.com>
2023-06-24 11:57:16 +0700

    -

--- predict_model.py
+++ predict_model.py
@@ -45,7 +45,7 @@ def predict_model(kode_saham, algoritma):
     start_date = datetime.date.today() if(tanggal_merah.check() or tanggal_merah.is_holiday() or tanggal_merah.is_sunday() or current_day == "Saturday") else start_date
 
     initial_date = "2017-01-01"
-    current_date = last_date.strftime("%Y-%m-%d") #datetime.date.today()
+    current_date = "2023-06-01" #last_date.strftime("%Y-%m-%d") #datetime.date.today()
     current_year = last_date.strftime("%Y")
     current_month = last_date.strftime("%m")
     current_day_date = last_date.strftime("%d")
